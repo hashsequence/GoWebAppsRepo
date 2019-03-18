b27ae90fe2daa939717b19d390c521ec622dabc0
Avery Wong <avwong13@g.ucla.edu>
2019-03-18 01:21:15 -0700

    Go Web Programming exercise, changed some lines of code to run locally on my pc

---
+++ go/bin/src/chitchat/data/data.go
@@ -0,0 +1,59 @@
+package data
+
+import (
+	"crypto/rand"
+	"crypto/sha1"
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"log"
+)
+
+const (
+  host     = "localhost"
+  port     =  5432
+  user     = "postgres"
+  password = "password"
+  dbname   = "chitchat"
+)
+
+var Db *sql.DB
+
+func init() {
+	var err error
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+ fmt.Println("db init at port ", port)
+	Db, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		fmt.Println("db failed")
+		log.Fatal(err)
+	}
+	fmt.Println("db success")
+	return
+}
+
+// create a random UUID with from RFC 4122
+// adapted from http://github.com/nu7hatch/gouuid
+func createUUID() (uuid string) {
+	u := new([16]byte)
+	_, err := rand.Read(u[:])
+	if err != nil {
+		log.Fatalln("Cannot generate UUID", err)
+	}
+
+	// 0x40 is reserved variant from RFC 4122
+	u[8] = (u[8] | 0x40) & 0x7F
+	// Set the four most significant bits (bits 12 through 15) of the
+	// time_hi_and_version field to the 4-bit version number.
+	u[6] = (u[6] & 0xF) | (0x4 << 4)
+	uuid = fmt.Sprintf("%x-%x-%x-%x-%x", u[0:4], u[4:6], u[6:8], u[8:10], u[10:])
+	return
+}
+
+// hash plaintext with SHA-1
+func Encrypt(plaintext string) (cryptext string) {
+	cryptext = fmt.Sprintf("%x", sha1.Sum([]byte(plaintext)))
+	return
+}
